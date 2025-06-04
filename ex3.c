1d26f88406d5abb97a3dc3cd285f826d4171d136
shaico111 <shai.co.new@gmail.com>
2025-06-04 18:20:13 +0300

    working

--- ex3.c
+++ ex3.c
@@ -2,9 +2,21 @@
 #include "ex3.h"
 
 void Ex3() {
-    // TODO: implement
+    char str[] = "Btwlzx Dqqes Eq|pj2 Tjhvqujs Iqoqjy bpg Eqfxtx Xcwwtt";
+    decode(str);
+    puts(str);
 }
 
 void decode(char* text) {
-    // TODO: implement
+    int location = 0;
+    int word_location = 1;
+    while (text[location] != '\0') {
+        if (text[location] != ' ') {
+            text[location] = CHAR_REPLACE(text[location], word_location);
+            word_location++;
+        } else {
+            word_location = 1;
+        }
+        location++;
+    }
 }
--- ex3.h
+++ ex3.h
@@ -1,4 +1,5 @@
 #pragma once
 #include "Headers.h"
+#define CHAR_REPLACE(CHAR,IDX) ((CHAR) - (IDX))
 void Ex3();
 void decode(char* text);
